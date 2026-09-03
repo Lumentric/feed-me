d84935eabb4df11739aae04b810351319c569c94
Ivan Bereznev <ione2ko@gmail.com>
2026-09-03 11:42:17 +0200

    Preserve status criteria when restoring hierarchy

--- src/fields/Entries.php
+++ src/fields/Entries.php
@@ -233,8 +233,8 @@ class Entries extends Field implements FieldInterface
         // we need to fill in the gaps, so that we know if the content has truly changed
         // https://github.com/craftcms/feed-me/issues/1418
         if ($foundElements && $maintainHierarchy && Plugin::$plugin->service->getConfig('compareContent', $this->feed['id'])) {
-            // get elements by IDs
-            $query = EntryElement::find()->id($foundElements);
+            // Get elements by IDs, use status from the original criteria in order to include disabled elements
+            $query = EntryElement::find()->status($criteria['status'])->id($foundElements);
             if (Craft::$app->getIsMultiSite()) {
                 $query->siteId = $criteria['siteId'] ?? Craft::$app->getSites()->getCurrentSite()->id;
             }
